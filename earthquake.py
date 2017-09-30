1d4bf23e3a4fa68a319040f5605bb8938e2306fa
Rajesh Goyal <rajesh.goyal@clcs.conduent.com>
2017-09-30 15:44:36 +0530

    Added parallel request implementations to speed up data gathering

--- earthquake.py
+++ earthquake.py
@@ -1,6 +1,7 @@
 import json
 import math
-import urllib3
+import asyncio
+import requests
 import datetime
 import matplotlib.pyplot as plot
 import pandas as pd
@@ -8,25 +9,21 @@ import pandas as pd
 
 class USGSService:
     """
-    Create a new `USGSService` object. `USGSService` takes optional arguments    
+    Create a new `USGSService` object. `USGSService` takes start and end date    
     that specify the behaviour of the `USGSService` object:
-    * `start_date`: Optional argument and accepts date in 'YYYY-MM-DD' format
-    * `end_date`: Optional argument and accepts date in 'YYYY-MM-DD' format    
+    * `start_date`: accepts date in 'YYYY-MM-DD' format
+    * `end_date`: accepts date in 'YYYY-MM-DD' format    
     """
 
     SERVICE_API_VERSION = '1.5.8'
     SERVICE_URL = "https://earthquake.usgs.gov/fdsnws/event/1/"
-    StartDate = '2016-01-01'
-    EndDate = '2016-01-02'
     LIMIT = 20000
     Offset = 1
     ServiceParams = {}
 
-    def __init__(self, start_date='', end_date=''):
-        if start_date != "":
-            self.StartDate = start_date
-        if end_date != "":
-            self.EndDate = end_date
+    def __init__(self, start_date, end_date):
+        self.StartDate = start_date
+        self.EndDate = end_date
 
     def check_api_version(self):
         """
@@ -35,7 +32,7 @@ class USGSService:
         """
         self.ServiceParams['format'] = 'quakeml'
         result = self.make_request('version')
-        result = result.data.decode('utf-8')
+        result = result.text
         if result != self.SERVICE_API_VERSION:
             raise Exception('Current USGS API Version : ' + result + ' Expected : ' + self.SERVICE_API_VERSION)
 
@@ -49,7 +46,7 @@ class USGSService:
         self.ServiceParams['starttime'] = self.StartDate
         self.ServiceParams['endtime'] = self.EndDate
         result = self.make_request('count')
-        return result.data.decode('utf-8')
+        return result.text
 
     def request_count(self):
         """
@@ -61,43 +58,6 @@ class USGSService:
         record_count = self.get_record_count()
         return math.ceil(float(record_count) / self.LIMIT)
 
-    def fetch_service(self):
-        """
-        Fetches all the results for API query
-        :return: 
-        List: List of response objects 
-        """
-        total_requests = self.request_count()
-        self.ServiceParams['format'] = 'geojson'
-        self.ServiceParams['starttime'] = self.StartDate
-        self.ServiceParams['endtime'] = self.EndDate
-        self.ServiceParams['limit'] = self.LIMIT
-        results = []
-        for x in range(total_requests):
-            self.ServiceParams['offset'] = self.Offset
-            result = self.make_request('query')
-            result = json.loads(result.data.decode('utf-8'))
-            results.append(result)
-            self.Offset = self.Offset + self.LIMIT
-
-        return results
-
-    def fetch_magnitude_data(self):
-        """
-        Fetches all the results for API query and collects magnitude data
-        :return: 
-        List: List of magnitudes data 
-        """
-        self.check_api_version()
-        results = self.fetch_service()
-        magnitudes = []
-        for result in results:
-            for row in result['features']:
-                if row["properties"].get("mag"):
-                    magnitudes.append(row['properties']['mag'])
-
-        return magnitudes
-
     def make_request(self, method):
         """
         Make API query request to USGS Service and return response
@@ -105,8 +65,50 @@ class USGSService:
         :return: 
         Object: Object of response
         """
-        http = urllib3.PoolManager()
-        return http.request('GET', self.SERVICE_URL + method, self.ServiceParams)
+        return requests.get(self.SERVICE_URL + method, self.ServiceParams)
+
+    def create_request_params(self):
+        """
+        Create a list of URLs params for building request 
+        :return: dictionary of params
+        """
+        params = []
+        param = {
+            'format': 'geojson',
+            'starttime': self.StartDate,
+            'endtime': self.EndDate,
+            'limit': self.LIMIT
+        }
+        total_requests = self.request_count()
+        for x in range(total_requests):
+            param['offset'] = self.Offset
+            params.append(param)
+            # increase offset to fetch next set of records
+            self.Offset = self.Offset + self.LIMIT
+
+        return params
+
+    async def make_async_request(self):
+        """
+        Make Async request to USGS Service and return response. 
+        :return: 
+        List: List of magnitude
+        """
+        loop = asyncio.get_event_loop()
+        params = self.create_request_params()
+        futures = [loop.run_in_executor(None, requests.get, self.SERVICE_URL + 'query', params[i]) for i in range(len(params))]
+
+        responses = []
+        magnitudes = []
+        for response in await asyncio.gather(*futures):
+            response = json.loads(response.text)
+            responses.append(response)
+            for result in responses:
+                for row in result['features']:
+                    if row["properties"].get("mag"):
+                        magnitudes.append(row['properties']['mag'])
+
+        return magnitudes
 
 
 def validate_date(date_text):
@@ -123,23 +125,17 @@ def validate_date(date_text):
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
 
 
-def main():
-    date_entry = input('Enter start date in YYYY-MM-DD format: ')
-    start_date = validate_date(date_entry)
-
-    date_entry = input('Enter end date in YYYY-MM-DD format: ')
-    end_date = validate_date(date_entry)
-
-    # Create an object of class USGSService to consume the USGS API
-    service = USGSService(start_date, end_date)
-    # collect all magnitide data for data visualization
-    magnitudes = service.fetch_magnitude_data()
+def visualize_earthquake(magnitudes):
+    """
+    Visualize earthquake magnitude data.
+    It generates pandas data frame and plot histogram
+    :param magnitudes: 
+    :return: 
+    """
 
     # Plot a histogram.
     n, bins, patch = plot.hist(magnitudes, histtype='step', range=(5.5, 9.5), bins=10)
-    plot.xlabel("Earthquake Magnitudes")
-    plot.ylabel("Frequency")
-    plot.title("Frequency by Magnitude")
+
     # Draw histogram of the DataFrame’s series
     histogram = pd.DataFrame()
     for i in range(0, len(n)):
@@ -151,9 +147,28 @@ def main():
     histogram.columns = ['Range of Magnitude', 'Frequency', 'Percentage']
     print(histogram)
 
-    # Plot Histogram from magnitude data
+    # Plot magnitude Histogram
+    plot.xlabel("Earthquake Magnitudes")
+    plot.ylabel("Frequency")
+    plot.title("Frequency by Magnitude")
     plot.show()
     plot.clf()
+
+
+def main():
+    date_entry = input('Enter start date in YYYY-MM-DD format: ')
+    start_date = validate_date(date_entry)
+
+    date_entry = input('Enter end date in YYYY-MM-DD format: ')
+    end_date = validate_date(date_entry)
+
+    # Create an object of class USGSService to consume the USGS API
+    service = USGSService(start_date, end_date)
+    # collect all magnitude data for data visualization
+    loop = asyncio.get_event_loop()
+    magnitudes = loop.run_until_complete(service.make_async_request())
+
+    visualize_earthquake(magnitudes)
 
 if __name__ == '__main__':
     main()
